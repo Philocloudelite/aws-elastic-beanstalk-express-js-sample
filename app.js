31f8a338322ff39b3a912d1a1f3383fde0c8dad4
philo-Essien <71226700+philo-Essien@users.noreply.github.com>
2021-09-23 16:42:32 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is myfirst Repo'));
+app.get('/', (req, res) => res.send('i love github and codecommit'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
